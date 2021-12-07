0abda7d381706eb35042773ee0ac67c44f3b7613
Daniel Castro <danielcastrofafe@gmail.com>
2021-12-07 11:40:37 +0000

    Added new assets and fixed snap drop zones gravity issues

--- Assets/Furniture Assembly/Scripts/OnSnapDestroyParentChild.cs
+++ Assets/Furniture Assembly/Scripts/OnSnapDestroyParentChild.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class OnSnapDestroyParentChild : MonoBehaviour
 {
+    [SerializeField] private GameObject nextStepObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnSnapDestroy() {
-        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    public void OnSnapDestroy()
+    {
+        Instantiate(nextStepObject, new Vector3((float)5.538, (float)0.691, (float)4.263), Quaternion.identity);
     }
 }
